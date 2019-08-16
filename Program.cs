d3c27f9f5d43c1661545b81c6965e96c434cf853
Mateusz Karcz <mateusz.karcz@interia.eu>
2019-08-16 20:10:25 +0200

    Project initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
+
+namespace Celones.DisplayManager {
+  class Program {
+    static void Main(string[] args) {
+      Pi.Init<BootstrapWiringPi>();
+      Console.WriteLine("Hello there~ :3");
+    }
+  }
+}
